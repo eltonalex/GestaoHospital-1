29eb670be1747c5cdba6ab994802f2ceb5a24ee1
Elton Alex Silva <eltonalex.s@gmail.com>
2019-04-01 12:23:06 -0300

    Contribuição para o projeto final considerando outras análises

---
+++ src/main/java/br/com/codenation/hospital/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package br.com.codenation.hospital.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import br.com.codenation.hospital.domain.Address;
+
+public interface AddressRepository extends MongoRepository<Address, String>{
+
+}
---
+++ src/main/java/br/com/codenation/hospital/repository/WarehouseRepository.java
@@ -0,0 +1,8 @@
+package br.com.codenation.hospital.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import br.com.codenation.hospital.domain.Warehouse;
+
+public interface WarehouseRepository extends MongoRepository<Warehouse, String>{
+
+}
